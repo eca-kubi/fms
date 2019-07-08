583075710d7cb784d85e6dc8ea39d58c6e26c2c8
Eric Clinton Appiah-Kubi <https://appiahmakuta70-outlook@dev.azure.com>
2019-07-08 11:46:19 +0000

    Added Near Miss Card

--- app/views/pages/start-page-hse.php
+++ app/views/pages/start-page-hse.php
@@ -2,8 +2,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php require_once APP_ROOT . '\views\includes\start-page-header.php'; ?>
-<div class="row">
+<!--<div class="row">
     <p class="h6 callout callout-danger center-notice animated flash">No apps are available at this time!</p>
+</div>-->
+<div class="row mbr-justify-content-center">
+    <div class="col-lg-4 mbr-col-md-10" data-url="<?php echo HOST . '/nearmiss'?>">
+        <div class="wrap">
+            <div class="ico-wrap">
+                <span class="mbr-iconfont fa-shield fa"></span>
+            </div>
+            <div class="text-wrap">
+                <h2 class="mbr-fonts-style mbr-bold mbr-section-title3 display-5 m-0">Near Miss<span></span></h2>
+                <p class="mbr-fonts-style text1 mbr-text desc">
+                    <span>Near miss draw</span>
+                    <span class="d-none">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum</span>
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
 </div>
 </section>
